75fa2fc42fed7bf37585b26049a65aa1ba2f341a
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-03-13 12:09:51 -0800

    redefine quest model.

--- models/quest.js
+++ models/quest.js
@@ -1,20 +1,30 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 // Create your Comments Model
 const commentSchema = new mongoose.Schema({
-  content: String,
-  }, {
-    timestamps: true
+  _id: String,
+  text: String,
+  userId: [{ // holds the ID of the user that submitted the comment
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  userName: String,
+  timestamps: true
 });
 
 // Create your Quest Model
 const questSchema = new mongoose.Schema({
+  _id: Number,
   name: String,
+  questItems: [],
+  rewardXp: Number,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
   comments: [commentSchema]
 });
 
 
-
-
 module.exports = mongoose.model('Quest', questSchema);
-module.exports = mongoose.model('Comment', commentSchema);
